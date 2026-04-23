375f44efb17afe90c9d40f488c50a2726e5c98f2
asifalam515 <asibulalam515@gmail.com>
2026-04-23 22:28:41 +0600

    auth  import issue fixed

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
 import { toNodeHandler } from "better-auth/node";
 import type { Application, Request, Response } from "express";
 import express from "express";
-import { auth } from "/lib/auth";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 const port = 5000; // The port your express server will be running on.
-app.all("/api/auth/*", toNodeHandler(auth));
+app.all("/api/auth", toNodeHandler(auth));
+app.all("/api/auth/*authPath", toNodeHandler(auth));
 // Enable URL-encoded form data parsing
 app.use(express.urlencoded({ extended: true }));
 
